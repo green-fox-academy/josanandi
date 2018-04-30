08c382b65864cda4f794f53850c9c048fd3d6d57
josanandi <andreea.josan@gmail.com>
2018-04-30 18:47:40 +0200

    Edited TicTacToe to Week04

--- Week-04/Day-2/src/TicTacToe.java
+++ Week-04/Day-2/src/TicTacToe.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
@@ -15,7 +23,60 @@ public class TicTacToe {
         System.out.println(ticTacResult("draw.txt"));
         // should print draw
     }
+
     public static String ticTacResult(String fileName) {
+        Path file = Paths.get(fileName);
+        List<String> readValues = null;
+        Character[][] table = new Character[3][3];
+        String mainResult = "";
+        boolean condition = true;
+
+        try {
+            readValues = Files.readAllLines(file);
+        } catch (IOException e) {
+            System.out.println("Unable to read the file!");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                table[i][j] = (readValues.get(i)).charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if ((table[i][0] == table[i][1]) && (table[i][0] == table[i][2])) {
+                condition = false;
+                mainResult = Character.toString(table[i][0]);
+                break;
+            }
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+            if (table[0][i] == table[1][i] && table[0][i] == table[2][i]) {
+                condition = false;
+                mainResult = Character.toString(table[0][i]);
+                break;
+            }
+        }
+
+        boolean testDiagonal = (table[0][0] == table[1][1]) && (table[0][0] == table[2][2]);
+        boolean testDiagonal2 = (table[0][2] == table[1][1]) && (table[0][2] == table[2][0]);
+
+        if (testDiagonal || testDiagonal2) {
+            condition = false;
+            mainResult = Character.toString(table[1][1]);
+        }
+
+
+        if (condition == true) {
+            mainResult = "draw";}
+
+            return mainResult;
+
+
 
     }
 }
+
+

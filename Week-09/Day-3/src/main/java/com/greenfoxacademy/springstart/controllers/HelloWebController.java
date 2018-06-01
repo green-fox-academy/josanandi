991c5049f66afa2527653646a0979e3967352229
josanandi <andreea.josan@gmail.com>
2018-06-02 00:43:31 +0200

    App build until Web Greet Counter App

--- Week-09/Day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ Week-09/Day-3/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,16 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class HelloWebController {
+    Greeting object =  new Greeting( "Andi");
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
-        model.addAttribute("name", " World");
+        model.addAttribute("name", object.getController());
+        object.incrementCount();
+        model.addAttribute("counter", object.getId());
         return "greeting";
     }
 

80f5b0b9552f7bd5401ed8dab7730e0970f78ced
josanandi <andreea.josan@gmail.com>
2018-06-11 20:16:33 +0200

    build HelloBeanWorld

---
+++ Week-11/Day-2/src/main/java/com/greenfoxacademy/diexercise/DiexerciseApplication.java
@@ -0,0 +1,22 @@
+package com.greenfoxacademy.diexercise;
+
+import com.greenfoxacademy.diexercise.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiexerciseApplication implements CommandLineRunner {
+    @Autowired
+    Printer printer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiexerciseApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        printer.log("Hello");
+    }
+}

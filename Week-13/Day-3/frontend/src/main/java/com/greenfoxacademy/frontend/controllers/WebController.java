10d27bd5c797523236d82529f1b3ba78762c225f
josanandi <andreea.josan@gmail.com>
2018-06-29 20:05:56 +0200

    (Frontend build)

---
+++ Week-13/Day-3/frontend/src/main/java/com/greenfoxacademy/frontend/controllers/WebController.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.frontend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+
+}

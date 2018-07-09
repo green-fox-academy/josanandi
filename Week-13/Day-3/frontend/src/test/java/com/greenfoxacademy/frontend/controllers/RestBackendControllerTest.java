1571ed8ad3841420ea24fb72e8c9c3f77f8485a3
josanandi <andreea.josan@gmail.com>
2018-07-09 20:58:40 +0200

    Frontend (one functioning test, GET/Doubling)

---
+++ Week-13/Day-3/frontend/src/test/java/com/greenfoxacademy/frontend/controllers/RestBackendControllerTest.java
@@ -0,0 +1,39 @@
+package com.greenfoxacademy.frontend.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.hamcrest.core.Is.is;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestBackendController.class)
+public class RestBackendControllerTest {
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testDoubling() throws Exception {
+        mockMvc.perform(get("/doubling")
+                .param("input", "4"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.result", is("8")));
+
+    }
+}
